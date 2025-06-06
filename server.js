79b373bd02651c0a678a21501a45c66cd95b8c2e
Butterszzz-art <armanbahalibusiness@gmail.com>
2025-06-06 17:10:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/config', (req, res) => {
   res.json({
-    airtableToken: process.env.AIRTABLE_TOKEN || '',
-    airtableBaseId: process.env.AIRTABLE_BASE_ID || ''
+    airtableToken: process.env.AIRTABLE_TOKEN || 'patHs7yemB2TYuOOc.6ed847f094d08b1d30710f9f5763d909d1841a2e7dc63fbdac208133a39ae577',
+    airtableBaseId: process.env.AIRTABLE_BASE_ID || 'appmjr4IgnEH72K1b'
   });
 });
 
